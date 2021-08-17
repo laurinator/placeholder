986be42aa79fb0dfe802b5b86fe2a87d7e3215d4
Laurin <laurin.mayer@web.de>
2021-08-17 22:36:43 +0200

    für niko

--- core/src/com/duodev/placeholder/CameraController.java
+++ core/src/com/duodev/placeholder/CameraController.java
@@ -10,25 +10,21 @@ public class CameraController {
     private final float cameraSpeed = 1f;
     private final float speedAdaptability = 50f;
 
-    private float realCameraX;
-    private float realCameraY;
-
     public CameraController(OrthographicCamera camera, Player player){
         this.camera = camera;
         this.camera.position.set(player.getMidpoint().x, player.getMidpoint().y, 0);
         this.player = player;
-        realCameraX = camera.position.x;
-        realCameraY = camera.position.y;
+
     }
 
     public void update(float delta){
 
-        float diffX = player.getMidpoint().x - realCameraX;
-        float diffY = player.getMidpoint().y - realCameraY;
+        float diffX = player.getMidpoint().x - camera.position.x;
+        float diffY = player.getMidpoint().y - camera.position.y;
 
-        realCameraX = realCameraX + (diffX * delta * cameraSpeed * (player.speed / speedAdaptability));
-        realCameraY = realCameraY + (diffY * delta * cameraSpeed * (player.speed / speedAdaptability));
-        camera.position.set(MathUtils.floor(realCameraX), MathUtils.floor(realCameraY), 0);
+        float newX = camera.position.x + (diffX * delta * cameraSpeed * (player.speed / speedAdaptability));
+        float newY = camera.position.y + (diffY * delta * cameraSpeed * (player.speed / speedAdaptability));
+        camera.position.set(newX, newY, 0);
 
     }
 
